59af2e5747cfcd15e25a285b65324d23c94bc16e
lucas <lucasmendozaguancha12@gmail.com>
2025-10-02 18:16:58 +0100

    Empezar a implementar member (está sin terminar)

--- src/org/ulpgc/is1/model/Member.java
+++ src/org/ulpgc/is1/model/Member.java
@@ -1,6 +1,8 @@
 package org.ulpgc.is1.model;
 import java.time.LocalDateTime;
 public class Member {
+    public Contact contact;
+    public Group group;
     public LocalDateTime fecha;
     public Member() {
         this.fecha = LocalDateTime.now();
